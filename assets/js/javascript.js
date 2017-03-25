212e1c96a8e12217b9fcb16f7aa548089c028f02
Erika Vidal <erikadianavidalcapillo@gmail.com>
2017-03-24 20:07:28 -0500

    refactorizando

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,11 +1,24 @@
-var array=  [{ nombre : "Arabela", apellido : "Rojas", rol : "T.A", cumpleaños : "23 de enero" },
-            { nombre : "Michelle", apellido : "Seguil", rol : "T.A", cumpleaños : "25 de noviembre" },
-            { nombre : "Meche", apellido : "Zubieta", rol : "T.A", cumpleaños : "02 de marzo" },
-            { nombre : "Papu", apellido : "Rivariola", rol : "Psicóloga", cumpleaños : "10 de octubre" },
-            { nombre : "Gian", apellido : "Corzo", rol : "Profesor", cumpleaños : "23 de enero" } ];
 
+  var personal=[{ nombre : "Arabela", apellido : "Rojas", rol : "T.A", cumpleaños : "23 de enero" },
+                { nombre : "Michelle", apellido : "Seguil", rol : "T.A", cumpleaños : "25 de noviembre" },
+                { nombre : "Meche", apellido : "Zubieta", rol : "T.A", cumpleaños : "02 de marzo" },
+                { nombre : "Papu", apellido : "Rivariola", rol : "Psicóloga", cumpleaños : "10 de octubre" },
+                { nombre : "Gian", apellido : "Corzo", rol : "Profesor", cumpleaños : "23 de enero" } ];
 
-  array.forEach(function(elemento){
-     document.write("<div><ul><li>Nombre :" + elemento.nombre +"</li><li>Apellido : "
-     + elemento.apellido + "</li><li>Rol : "+ elemento.rol + "</li><li>Cumpleaños : " + elemento.cumpleaños +"</li></ul></div>");
-     });
+  var contenedor=document.getElementById("datos");
+
+  personal.forEach(function(elemento){
+
+    var divContenedor=document.createElement("div");
+    var ulContenedor=document.createElement("ul");
+    divContenedor.classList.add("contenedor");
+
+    for(var prop in elemento){
+
+      var liContenedor=document.createElement("li");
+      liContenedor.innerHTML=prop+ ":" + elemento[prop];
+      ulContenedor.appendChild(liContenedor);
+      divContenedor.appendChild(ulContenedor);
+    }
+      contenedor.appendChild(divContenedor);
+  });
